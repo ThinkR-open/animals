5ade11bfbe66b57b8a80c339670daa24d32d1514
Romain Francois <romain@r-enthusiasts.com>
2016-09-25 00:50:57 +0200

    less titles

--- R/animals.R
+++ R/animals.R
@@ -33,135 +33,115 @@ scrap_all <- function(what = "bebe,chats", .progress = "text", ...){
   unlist(res)
 }
 
-#' urls for pictures of cats
-#' 
+#' urls for pictures of animals
 #' @export
 #' @rdname animals
 cats <- function(){
   scrap_all("chats")
 }
   
-#' urls for pictures of kittens
-#' 
 #' @export
 #' @rdname animals
 kittens <- function(){
   scrap_all("bebe,chats")
 }
 
-#' urls for pictures of dogs
 #' @export
 #' @rdname animals
 dogs <- function(){
   scrap_all("chiens")
 }
 
-#' urls for pictures of puppies
 #' @export
 #' @rdname animals
 puppies <- function(){
   scrap_all("bebe,chiens")
 }
 
-#' urls for pictures of horses
 #' @export
 #' @rdname animals
 horses <- function(){
   scrap_all("chevaux")
 }
 
-#' urls for pictures of foals
 #' @export
 #' @rdname animals
 foals <- function(){
   scrap_all("bebe,chevaux")
 }
 
-#' urls for pictures of rodents
 #' @export
 #' @rdname animals
 rodents <- function(){
   scrap_all("rongeurs")
 }
 
-#' urls for pictures of rodents
 #' @export
 #' @rdname animals
 baby_rodents <- function(){
   scrap_all("bebe,rongeurs")
 }
 
-#' urls for pictures of birds
 #' @export
 #' @rdname animals
 birds <- function(){
   scrap_all("oiseaux")
 }
 
-#' urls for pictures of chicks
 #' @export
 #' @rdname animals
 chicks <- function(){
   scrap_all("bebe,oiseaux")
 }
 
-#' urls for pictures of arthropods
 #' @export
 #' @rdname animals
 arthropods <- function(){
   scrap_all("arthropodes")
 }
 
-#' urls for pictures of farm animals
 #' @export
 #' @rdname animals
 farm_animals <- function(){
   scrap_all("animaux-de-la-ferme")
 }
 
-#' urls for pictures of farm animals
 #' @export
 #' @rdname animals
 baby_farm_animals <- function(){
   scrap_all("bebe,animaux-de-la-ferme")
 }
 
-#' urls for pictures of reptiles
 #' @export
 #' @rdname animals
 reptiles <- function(){
   scrap_all("reptiles")
 }
 
-#' urls for sea animals pictures
 #' @export
 #' @rdname animals
 sea_animals <- function(){
   scrap_all("animaux-aquatiques")
 }
 
-#' urls for pictures of other animals
 #' @export
 #' @rdname animals
 other_animals <- function(){
   scrap_all("autres")
 }
 
-#' urls for other baby animals pictures
 #' @export
 #' @rdname animals
 other_baby_animals <- function(){
   scrap_all("bebe,autres")
 }
 
-#' urls for all animal pictures
 #' @export
 #' @rdname animals
 all_animals <- function(){
   scrap_all("toutes")
 }
 
-#' urls for all baby animal pictures
 #' @export
 #' @rdname animals
 all_baby_animals <- function(){
